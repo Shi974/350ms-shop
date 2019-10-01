2014c4c6f55004f7965d52a3ad8587c3ca4e9e05
Shi974 <42869389+Shi974@users.noreply.github.com>
2019-10-01 15:28:03 +0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    350ms Shop
                 </div>
 
                 <div class="links">
